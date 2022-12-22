a00466fa7a74458e5537c87ca12bcc71510ec8f2
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2022-12-22 23:48:33 +0100

    Fix stylelint and eslint errors

--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
-import "./reset.css";
-import "./style.css";
-import dots from "./images/dots.png";
-import refresh from "./images/refresh.png";
-import enter from "./images/enter.png";
+import './reset.css';
+import './style.css';
+import dots from './images/dots.png';
+import refresh from './images/refresh.png';
+import enter from './images/enter.png';
 
-const listContainer = document.querySelector(".list");
-const enterImg = document.querySelector("#enter");
-const refreshImg = document.querySelector("#refresh");
+const listContainer = document.querySelector('.list');
+const enterImg = document.querySelector('#enter');
+const refreshImg = document.querySelector('#refresh');
 
-let lists = [
+const lists = [
   {
-    description: "todo1",
-    completed: "true",
-    index: "1",
+    description: 'todo1',
+    completed: 'true',
+    index: '1',
   },
   {
-    description: "todo2",
-    completed: "false",
-    index: "2",
+    description: 'todo2',
+    completed: 'false',
+    index: '2',
   },
   {
-    description: "todo3",
-    completed: "true",
-    index: "3",
+    description: 'todo3',
+    completed: 'true',
+    index: '3',
   },
   {
-    description: "todo4",
-    completed: "false",
-    index: "4",
+    description: 'todo4',
+    completed: 'false',
+    index: '4',
   },
 ];
 
 const showList = () => {
   lists.forEach((i) => {
-    const listElt = document.createElement("li");
+    const listElt = document.createElement('li');
     listElt.dataset.listId = i.index;
-    listElt.classList.add("checkbox");
+    listElt.classList.add('checkbox');
     listElt.innerHTML += `<span><input type="checkbox" /> ${i.description}</span><img src="${dots}" />`;
-    enterImg.setAttribute("src", enter);
-    refreshImg.setAttribute("src", refresh);
+    enterImg.setAttribute('src', enter);
+    refreshImg.setAttribute('src', refresh);
     listContainer.appendChild(listElt);
   });
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: {
-    index: "./src/index.js",
+    index: './src/index.js',
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
-    static: "./dist",
+    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
   ],
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
   optimization: {
-    runtimeChunk: "single",
+    runtimeChunk: 'single',
   },
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
     ],
   },

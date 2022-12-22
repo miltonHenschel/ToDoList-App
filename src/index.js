a75bd79af76c8a062deecc0d97c1938b3128f150
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2022-12-23 00:42:24 +0100

    Create array of list objs

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import "./reset.css";
 import "./style.css";
+
+const lists = [
+  {
+    description: "todo1",
+    completed: "true",
+    index: "1",
+  },
+  {
+    description: "todo2",
+    completed: "false",
+    index: "2",
+  },
+  {
+    description: "todo3",
+    completed: "true",
+    index: "3",
+  },
+  {
+    description: "todo4",
+    completed: "false",
+    index: "4",
+  },
+];

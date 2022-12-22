3ae8689a0dd04527c660bf510ec350b60a6c668e
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2022-12-22 17:23:00 +0100

    When enter key pressed, add to do list item

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
+import "./reset.css";
 import "./style.css";
+
+let list = document.querySelector(".list");
+const form = document.querySelector("#form");
+const text = document.querySelector("#text");
+
+class ToDoList {
+  constructor(desc, complete, index) {
+    this.desc = desc;
+    this.complete = complete;
+    this.index = index;
+  }
+}
+
+form.addEventListener("keypress", (e) => {
+  if (13 == e.keyCode) {
+    console.log("input!");
+    e.preventDefault();
+    const val = text.value;
+    list.innerHTML += `<div class="checkbox" id=""><input type="checkbox" />${val}</div>`;
+    form.reset();
+  }
+});

a1ba2c5b58bf5d13af83a5851b9045d25b15eabd
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2022-12-22 23:45:32 +0100

    Add array of list obj, populate it and load it dynamically

--- src/index.js
+++ src/index.js
@@ -1,24 +1,46 @@
 import "./reset.css";
 import "./style.css";
+import dots from "./images/dots.png";
+import refresh from "./images/refresh.png";
+import enter from "./images/enter.png";
 
-let list = document.querySelector(".list");
-const form = document.querySelector("#form");
-const text = document.querySelector("#text");
+const listContainer = document.querySelector(".list");
+const enterImg = document.querySelector("#enter");
+const refreshImg = document.querySelector("#refresh");
 
-class ToDoList {
-  constructor(desc, complete, index) {
-    this.desc = desc;
-    this.complete = complete;
-    this.index = index;
-  }
-}
+let lists = [
+  {
+    description: "todo1",
+    completed: "true",
+    index: "1",
+  },
+  {
+    description: "todo2",
+    completed: "false",
+    index: "2",
+  },
+  {
+    description: "todo3",
+    completed: "true",
+    index: "3",
+  },
+  {
+    description: "todo4",
+    completed: "false",
+    index: "4",
+  },
+];
 
-form.addEventListener("keypress", (e) => {
-  if (13 == e.keyCode) {
-    console.log("input!");
-    e.preventDefault();
-    const val = text.value;
-    list.innerHTML += `<div class="checkbox" id=""><input type="checkbox" />${val}</div>`;
-    form.reset();
-  }
-});
+const showList = () => {
+  lists.forEach((i) => {
+    const listElt = document.createElement("li");
+    listElt.dataset.listId = i.index;
+    listElt.classList.add("checkbox");
+    listElt.innerHTML += `<span><input type="checkbox" /> ${i.description}</span><img src="${dots}" />`;
+    enterImg.setAttribute("src", enter);
+    refreshImg.setAttribute("src", refresh);
+    listContainer.appendChild(listElt);
+  });
+};
+
+showList();
--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,10 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
     ],
   },
 };

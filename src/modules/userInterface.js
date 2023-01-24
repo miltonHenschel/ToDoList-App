54602e452caf71e03d29e67685555aae67dc2a1f
Henschel NKETCHOGUE M <nketchoguemilton@gmail.com>
2023-01-24 19:16:59 +0100

    Remove commented code

--- src/modules/userInterface.js
+++ src/modules/userInterface.js
@@ -44,31 +44,6 @@ class UserInterface {
     });
   }
 
-  // // COMPLETED TO DO lIST
-  // static compToDoList() {
-  //   const checkbox = document
-  //     .querySelector('#list')
-  //     .querySelectorAll('.checkbox');
-
-  //   checkbox.forEach((elt) => {
-  //     elt.addEventListener('change', (e) => {
-  //       LocalStorage.taskComp(e.target.parentElement.parentElement.id);
-  //     });
-  //   });
-  // }
-
-  // // CLEAR COMPLETED TO DO LIST
-  // static clearComp() {
-  //   const clearButton = document.querySelector('.clear');
-  //   clearButton.addEventListener('click', () => {
-  //     let todolists = LocalStorage.getToDoLists();
-  //     todolists = todolists.filter((elt) => elt.comp !== true);
-  //     localStorage.setItem('todolists', JSON.stringify(todolists));
-  //     window.location.reload();
-  //     LocalStorage.resetIndex();
-  //   });
-  // }
-
   static addToDoLists(item) {
     const listContent = document.querySelector('#list');
     const listElt = document.createElement('li');
